2a919e052d48eb8a9370990d52a5940b6d823ea8
Sufiandy Elmy <elmysufiandy09@gmail.com>
2020-05-10 14:06:55 +0700

    New add

--- CoreML Vision/ViewController.swift
+++ CoreML Vision/ViewController.swift
@@ -7,14 +7,82 @@
 //
 
 import UIKit
+import AVFoundation
+import Vision
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
+    let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Label"
+        label.font = label.font.withSize(30)
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupCaptureSession()
+        
+        view.addSubview(label)
+        setupLabel()
     }
-
-
+    
+    
+    
+    func setupCaptureSession() {
+        
+        // creates a new capture session
+        let captureSession = AVCaptureSession()
+        
+        // search for available capture devices
+        let availableDevices = AVCaptureDevice.DiscoverySession(deviceTypes: [.builtInWideAngleCamera], mediaType: AVMediaType.video, position: .back).devices
+        
+        // get capture device, add device input to capture session
+        do {
+            if let captureDevice = availableDevices.first {
+                captureSession.addInput(try AVCaptureDeviceInput(device: captureDevice))
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        
+        // setup output, add output to capture session
+        let captureOutput = AVCaptureVideoDataOutput()
+        captureSession.addOutput(captureOutput)
+        
+        captureOutput.setSampleBufferDelegate(self, queue: DispatchQueue(label: "videoQueue"))
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.frame
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        captureSession.startRunning()
+    }
+    
+    // called everytime a frame is captured
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        guard let model = try? VNCoreMLModel(for: Resnet50().model) else { return }
+        let request = VNCoreMLRequest(model: model) { (finishedRequest, error) in
+            guard let results = finishedRequest.results as? [VNClassificationObservation] else { return }
+            guard let Observation = results.first else { return }
+            
+            DispatchQueue.main.async(execute: {
+                self.label.text = "\(Observation.identifier)"
+                print(Observation.confidence)
+            })
+        }
+        guard let pixelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else { return }
+        
+        // executes request
+        try? VNImageRequestHandler(cvPixelBuffer: pixelBuffer, options: [:]).perform([request])
+    }
+    
+    func setupLabel() {
+        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
+    }
 }
 
