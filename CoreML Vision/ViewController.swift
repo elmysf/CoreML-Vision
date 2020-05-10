cc30fe7b4cd2ef6ba6d85ec1026717a330cfa588
Sufiandy Elmy <elmysufiandy09@gmail.com>
2020-05-10 13:56:38 +0700

    Initial Commit

---
+++ CoreML Vision/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreML Vision
+//
+//  Created by Sufiandy Elmy on 10/05/20.
+//  Copyright © 2020 Sufiandy Elmy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
